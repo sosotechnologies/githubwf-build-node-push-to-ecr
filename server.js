1d0caec998464f61ebe485633638fa25f1069de5
sosotechnologies <sosotech2000@gmail.com>
2024-07-04 15:23:55 -0400

    tester

--- server.js
+++ server.js
@@ -1,46 +1,100 @@
+// before rum
+//npm install express
+
+// to run 
+// npm start
+
+'use strict';
+
 const express = require('express');
-const bodyParser = require('body-parser');
 
+// Constants
+const PORT = 8088;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'UNITY-XCITE';
+
+
+// App
 const app = express();
-
-let userGoal = 'We Know Technology!';
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
-
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <link rel="stylesheet" href="styles.css">
-      </head>
-      <body>
-        <section>
-          <h2>SosoTech</h2>
-          <h3>${userGoal}</h3>
-        </section>
-        <form action="/store-goal" method="POST">
-          <div class="form-control">
-            <label>What's our Course Goal?</label>
-            <input type="text" name="goal">
-          </div>
-          <button>Set expectations</button>
-        </form>
-      </body>
-    </html>
-  `);
+  res.statusCode = 200;
+  const msg = 'Hello from XCite, lets keep xxiting';
+  res.send(getPage(msg));
 });
 
-app.post('/store-goal', (req, res) => {
-  const enteredGoal = req.body.goal;
-  console.log(enteredGoal);
-  userGoal = enteredGoal;
-  res.redirect('/');
+app.get('/test', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from /test Node!';
+  res.send(getPage(msg));
 });
 
-app.listen(8089);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+function getPage(message) {
+
+  let body = "<!DOCTYPE html>\n"
+    + "<html>\n"
+    + "<style>\n"
+    + "body, html {\n"
+    + "  height: 100%;\n"
+    + "  margin: 0;\n"
+    + "}\n"
+    + "\n"
+    + ".bgimg {\n"
+    + "  background-image: url('https://www.w3schools.com/w3images/lights.jpg');\n"
+    + "  height: 100%;\n"
+    + "  background-position: center;\n"
+    + "  background-size: cover;\n"
+    + "  position: relative;\n"
+    + "  color: white;\n"
+    + "  font-family: \"Courier New\", Courier, monospace;\n"
+    + "  font-size: 25px;\n"
+    + "}\n"
+    + "\n"
+    + ".topleft {\n"
+    + "  position: absolute;\n"
+    + "  top: 0;\n"
+    + "  left: 16px;\n"
+    + "}\n"
+    + "\n"
+    + ".bottomleft {\n"
+    + "  position: absolute;\n"
+    + "  bottom: 0;\n"
+    + "  left: 16px;\n"
+    + "}\n"
+    + "\n"
+    + ".middle {\n"
+    + "  position: absolute;\n"
+    + "  top: 50%;\n"
+    + "  left: 50%;\n"
+    + "  transform: translate(-50%, -50%);\n"
+    + "  text-align: center;\n"
+    + "}\n"
+    + "\n"
+    + "hr {\n"
+    + "  margin: auto;\n"
+    + "  width: 40%;\n"
+    + "}\n"
+    + "</style>\n"
+    + "<body>\n"
+    + "\n"
+    + "<div class=\"bgimg\">\n"
+    + "  <div class=\"topleft\">\n"
+    + "    <p>ENVIRONMENT: " + ENV + "</p>\n"
+    + "  </div>\n"
+    + "  <div class=\"middle\">\n"
+    + "    <h1>Host/container name</h1>\n"
+    + "    <hr>\n"
+    + "    <p>" + OS.hostname() + "</p>\n"
+    + "  </div>\n"
+    + "  <div class=\"bottomleft\">\n"
+    + "    <p>" + message + "</p>\n"
+    + "  </div>\n"
+    + "</div>\n"
+    + "\n"
+    + "</body>\n"
+    + "</html>\n";
+  return body;
+}
