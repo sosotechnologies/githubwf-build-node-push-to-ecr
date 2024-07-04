44522c5245ebc947191e62c56141818c6f16d646
sosotechnologies <sosotech2000@gmail.com>
2024-07-04 15:17:49 -0400

    tester

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const OS = require('os');
 
-// Constants
-const PORT = 8088;
-const HOST = '0.0.0.0';
-const ENV = 'UNITY-XCITE';
-
-// Initialize Express
 const app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
-
-// Variables
 let userGoal = 'We Know Technology!';
 
-// Routes
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = userGoal;
-  res.send(getPage(msg));
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>SosoTech</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>What's our Course Goal?</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set expectations</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
 app.post('/store-goal', (req, res) => {
@@ -33,74 +43,4 @@ app.post('/store-goal', (req, res) => {
   res.redirect('/');
 });
 
-app.get('/test', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
-  res.send(getPage(msg));
-});
-
-// Start the server
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
-
-// Function to generate HTML page
-function getPage(message) {
-  return `
-    <!DOCTYPE html>
-    <html>
-    <style>
-      body, html {
-        height: 100%;
-        margin: 0;
-      }
-      .bgimg {
-        background-image: url('https://www.w3schools.com/w3images/lights.jpg');
-        height: 100%;
-        background-position: center;
-        background-size: cover;
-        position: relative;
-        color: white;
-        font-family: "Courier New", Courier, monospace;
-        font-size: 25px;
-      }
-      .topleft {
-        position: absolute;
-        top: 0;
-        left: 16px;
-      }
-      .bottomleft {
-        position: absolute;
-        bottom: 0;
-        left: 16px;
-      }
-      .middle {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
-      }
-      hr {
-        margin: auto;
-        width: 40%;
-      }
-    </style>
-    <body>
-      <div class="bgimg">
-        <div class="topleft">
-          <p>ENVIRONMENT: ${ENV}</p>
-        </div>
-        <div class="middle">
-          <h1>Host/container name</h1>
-          <hr>
-          <p>${OS.hostname()}</p>
-        </div>
-        <div class="bottomleft">
-          <p>${message}</p>
-        </div>
-      </div>
-    </body>
-    </html>
-  `;
-}
+app.listen(80);
