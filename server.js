0ec2d11af830a54a2430347bd116557d90ccce13
sosotechnologies <sosotech2000@gmail.com>
2024-07-04 21:41:12 -0400

    testeere

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'UNITY-XCITE';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from XCite, lets keep testing';
+  const msg = 'Hello from XCite, lets keep Xxiting';
   res.send(getPage(msg));
 });
 
