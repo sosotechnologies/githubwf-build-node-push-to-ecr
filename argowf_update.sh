66a08387a4fc02ae0649b3acecba117bdac50eed
sosotechnologies <sosotech2000@gmail.com>
2024-07-05 06:47:50 -0400

    versioning

--- argowf_update.sh
+++ argowf_update.sh
@@ -1,16 +1,16 @@
-# # #!/bin/bash
-
-# # # Read the new version
-# # new_version=$(cat VERSION)
-
-# # # Update the argowf.yaml file
-# # sed -i "s/version: \"\"/version: \"$new_version\"/" argowf.yaml
-
-
 # #!/bin/bash
 
 # # Read the new version
 # new_version=$(cat VERSION)
 
 # # Update the argowf.yaml file
-# sed -i "s/version: \"[0-9.]*\"/version: \"$new_version\"/" argowf.yaml
+# sed -i "s/version: \"\"/version: \"$new_version\"/" argowf.yaml
+
+
+#!/bin/bash
+
+# Read the new version
+new_version=$(cat VERSION)
+
+# Update the argowf.yaml file
+sed -i "s/version: \"[0-9.]*\"/version: \"$new_version\"/" argowf.yaml
