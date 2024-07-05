97601c524635c6d13f9a5cac5c2891c4b095020e
sosotechnologies <sosotech2000@gmail.com>
2024-07-05 13:21:48 -0400

    switching to gh-registry

--- build-push-to-gh/bump_version.sh
+++ build-push-to-gh/bump_version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Check if VERSION file exists
-if [ ! -f VERSION ]; then
-  echo "2.2.19" > VERSION
+if [ ! -f VERSION-GH ]; then
+  echo "2.2.19" > VERSION-GH
 fi
 
-current_version=$(cat VERSION)
+current_version=$(cat VERSION-GH)
 
 IFS='.' read -r -a version_parts <<< "$current_version"
 version_parts[2]=$((version_parts[2] + 1))
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 
 # Write the new version back to the VERSION file
-echo $new_version > VERSION
+echo $new_version > VERSION-GH
 echo $new_version
