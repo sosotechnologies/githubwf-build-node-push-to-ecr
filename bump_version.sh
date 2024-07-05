28a305ebb2e23a53c44b013c79c65a147a75fef9
sosotechnologies <sosotech2000@gmail.com>
2024-07-05 06:06:19 -0400

    versioning

--- bump_version.sh
+++ bump_version.sh
@@ -2,16 +2,16 @@
 
 # Check if VERSION file exists
 if [ ! -f VERSION ]; then
-  echo "1.0" > VERSION
+  echo "2.2.11" > VERSION
 fi
 
 # Read the current version
 current_version=$(cat VERSION)
 
-# Increment the version number
+# Increment the patch version number
 IFS='.' read -r -a version_parts <<< "$current_version"
-version_parts[1]=$((version_parts[1] + 1))
-new_version="${version_parts[0]}.${version_parts[1]}"
+version_parts[2]=$((version_parts[2] + 1))
+new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 
 # Write the new version back to the VERSION file
 echo $new_version > VERSION
