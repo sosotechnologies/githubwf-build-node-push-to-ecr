4d28f9cb822d7ad0c162baf68224fd9294767851
sosotechnologies <sosotech2000@gmail.com>
2024-07-05 07:02:37 -0400

    versioning-2

--- argowf_update.sh
+++ argowf_update.sh
@@ -1,12 +1,3 @@
-# #!/bin/bash
-
-# # Read the new version
-# new_version=$(cat VERSION)
-
-# # Update the argowf.yaml file
-# sed -i "s/version: \"\"/version: \"$new_version\"/" argowf.yaml
-
-
 #!/bin/bash
 
 # Read the new version
--- bump_version.sh
+++ bump_version.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Check if VERSION file exists
 if [ ! -f VERSION ]; then
   echo "2.2.11" > VERSION
 fi
 
-# Read the current version
 current_version=$(cat VERSION)
 
-# Increment the patch version number
 IFS='.' read -r -a version_parts <<< "$current_version"
 version_parts[2]=$((version_parts[2] + 1))
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
