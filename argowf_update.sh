a5bd15fd42b5e7dcac5fb7cdb0b32888811373b9
sosotechnologies <sosotech2000@gmail.com>
2024-07-05 07:08:22 -0400

    versioning-2

--- argowf_update.sh
+++ argowf_update.sh
@@ -3,5 +3,8 @@
 # Read the new version
 new_version=$(cat VERSION)
 
-# Update the argowf.yaml file
+# Update the argowf.yaml file with the new version
 sed -i "s/version: \"[0-9.]*\"/version: \"$new_version\"/" argowf.yaml
+
+# Print the updated argowf.yaml for verification
+cat argowf.yaml
